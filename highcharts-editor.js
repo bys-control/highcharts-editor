2898e03e42271d657dcc17b476aca0c5c4c64743
cpfarher <c.pfarher@gmail.com>
2016-12-27 16:53:32 -0300

    first commit

---
+++ highcharts-editor.js
@@ -0,0 +1,5 @@
+module.exports = function () {
+	require 'files/highcharts-editor.min.js'
+	require 'files/highcharts-editor.min.css'
+	require 'files/highcharts-editor.advanced.min.js'
+}
---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('highcharts-editor.js');
